a2c7b7f4e84f0eeaa73057dc0e2f010dd7012713
xtravers <legoxavierlocketravers@gmail.com>
2022-10-05 23:08:02 +1100

    work on adding final-ish unweighted ranking

--- scripts/utilities/rank_utilities.py
+++ scripts/utilities/rank_utilities.py
@@ -1,7 +1,6 @@
 ''' Functions and utilities to rank data on different properties
 TODO: Further commenting
 '''
-from h11 import Data
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -9,7 +8,7 @@ from pandas import DataFrame
 from utilities.log_utilities import logger
 
 def add_column_rank(df: DataFrame, colname: str, ascending: bool = False,
-    pct: bool = False) -> DataFrame:
+    rank_type: str = 'rank') -> DataFrame:
     """ Add a new column for the rank of another column in the given df.
     This really exists so the column naming is kept standard.
 
@@ -24,26 +23,36 @@ def add_column_rank(df: DataFrame, colname: str, ascending: bool = False,
     Returns:
         `DataFrame`: The modified Pandas `DataFrame` containing the new rank column
     """
-    rank_colname = f"{colname}_{'pctrank' if pct else 'rank'}"
-    df[rank_colname] = df[colname].rank(
-        method='average',
-        ascending=ascending,
-        pct=pct,
-        na_option='bottom'
-    )
+    rank_colname = f'{colname}_{rank_type}'
+    if rank_type == 'minmax':
+        df[rank_colname] = min_max_scale(df[colname])
+        if not ascending:
+            df[rank_colname] = 1 - df[rank_colname]
+    else:
+        pct = True if rank_type == 'pct' else False
+        df[rank_colname] = df[colname].rank(
+            method='average',
+            ascending=ascending,
+            pct=pct,
+            na_option='bottom'
+        )
     return df
 
-
+# min_max_scale
+def min_max_scale(column):
+    #data = [i for i in data if np.isnan(i) == False]
+    if np.max(column) == np.min(column) : return np.ones((len(column), 1)) / 2
+    else : return (column - np.min(column)) / (np.max(column) - np.min(column))
 
 def average_rank(df: DataFrame, colnames: 'list[str]', 
-        pct: bool = False, weights: 'list[float]|None' = None) -> DataFrame:
+        rank_type: str = 'rank', weights: 'list[float]|None' = None) -> DataFrame:
 
     n = len(colnames)
     if weights == None:
         weights = np.ones((len(colnames), 1)) / n
 
     rank_colnames = [
-        f"{cn}_{'pctrank' if pct else 'rank'}" for cn in colnames
+        f'{cn}_{rank_type}' for cn in colnames
     ]
 
     df['average_rank'] = np.matrix(df[rank_colnames]) @ weights
